c8b9dea0b518481bab96d0afbf29d1b3bfdebd77
coder <yamatcha.pixelforge@gmail.com>
2026-02-10 03:40:40 +0900

    feat(nvim): operation settings in init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,9 +1,23 @@
+-- share clipboard with OS
 vim.opt.clipboard:append('unnamedplus,unnamed')
 
+-- use 2-spaces indent
+vim.opt.expandtab = true
+vim.opt.shiftround = true
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.tabstop = 2
+
+-- scroll offset as 3 lines
+vim.opt.scrolloff = 3
+
+-- move the cursor to the previous/next line across the first/last character
+vim.opt.whichwrap = 'b,s,h,l,<,>,[,],~'
+
 vim.api.nvim_create_user_command(
-    'InitLua',
-    function()
-	vim.cmd.edit(vim.fn.stdpath('config') .. '/init.lua')
-    end,
-    {}
+  'InitLua',
+  function()
+    vim.cmd.edit(vim.fn.stdpath('config') .. '/init.lua')
+  end,
+  { desc = 'Open init.lua' }
 )
