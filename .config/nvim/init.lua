e94855dc3c2807380576ec0a6b78a7ad0f09338b
coder <yamatcha.pixelforge@gmail.com>
2026-02-09 16:16:51 +0900

    feat(nvim): init.lua

---
+++ .config/nvim/init.lua
@@ -0,0 +1,9 @@
+vim.opt.clipboard:append('unnamedplus,unnamed')
+
+vim.api.nvim_create_user_command(
+    'InitLua',
+    function()
+	vim.cmd.edit(vim.fn.stdpath('config') .. '/init.lua')
+    end,
+    {}
+)
