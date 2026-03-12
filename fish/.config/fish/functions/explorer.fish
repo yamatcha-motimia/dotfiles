6fb58f47bf0d9316c1d15685f3b836666ad0a0c5
coder <yamatcha.pixelforge@gmail.com>
2026-03-12 23:27:04 +0900

    feat(fish): enable arguments for explorer command

--- fish/.config/fish/functions/explorer.fish
+++ fish/.config/fish/functions/explorer.fish
@@ -1,3 +1,15 @@
 function explorer --description "Open current directory in Windows Explorer"
-	/mnt/c/Windows/explorer.exe (wslpath -w (pwd))
+	# 変数 target に対象のパスを格納
+	set target $argv[1]
+	test -z "$target"; and set target $PWD
+
+	# 入力された引数の判定 
+	if not test -d $target
+		echo "explorer: path does not exist: $target" >&2
+		return 1
+	end
+
+	# explorer.exe は正常動作で 1 を返すため 0 に変換
+	/mnt/c/Windows/explorer.exe (wslpath -w $target)
+	or test $status -eq 1
 end
